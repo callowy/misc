993d299c507362b6d48ca35ff19727604c6b3e9c
callowy <69256954+callowy@users.noreply.github.com>
2020-10-24 16:15:09 -0700

    Add files via upload

--- estimate_pi.R
+++ estimate_pi.R
@@ -1,6 +1,8 @@
 estimate_pi <- function(n) {
-
+  # Calculate distance from origin
   z <- mapply(function(x,y) x^2 + y^2, runif(n), runif(n))
 
+  # (area of circle / area of square) = (points in z with distance < 1 / all points in z)
+  # points within unit circle / points within unit square
   4 * (length(z[z < 1]) / length(z))
 }
