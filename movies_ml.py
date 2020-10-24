7a0834567e9fc66cf2d9b602254c966ce1e3af0b
callowy <69256954+callowy@users.noreply.github.com>
2020-10-24 16:06:55 -0700

    Add files via upload

---
+++ movies_ml.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  9 12:42:23 2020
+
+@author: 16025
+"""
+import pandas as pd
+import numpy as np
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn import metrics
+
+
